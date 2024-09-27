1fb37cd6d3287700697a560cbaba2579113e9182
jeslifs <97340330+jeslifs@users.noreply.github.com>
2024-09-27 23:22:20 +0530

    random particle size

--- src/script.js
+++ src/script.js
@@ -142,6 +142,7 @@ const particles = {}
 
 // Geometry
 const particlesUvArray = new Float32Array(baseGeometry.count * 2)
+const sizesArray = new Float32Array(baseGeometry.count)
 
 for(let y = 0; y < gpgpu.size; y++)
 {
@@ -150,11 +151,15 @@ for(let y = 0; y < gpgpu.size; y++)
         const i = (y * gpgpu.size) + x
         const i2 = i * 2
 
+        // Particles Uv
         const uvX = (x + 0.5) / gpgpu.size
         const uvY = (y + 0.5) / gpgpu.size
 
         particlesUvArray[i2] = uvX
         particlesUvArray[i2 + 1] = uvY
+
+        // Sizes
+        sizesArray[i] = Math.random()
     }
 }
 
@@ -162,6 +167,7 @@ particles.geometry = new THREE.BufferGeometry()
 particles.geometry.setDrawRange(0, baseGeometry.count)
 particles.geometry.setAttribute('aParticlesUv', new THREE.BufferAttribute(particlesUvArray, 2))
 particles.geometry.setAttribute('aColor', baseGeometry.instance.attributes.color)
+particles.geometry.setAttribute('aSize', new THREE.BufferAttribute(sizesArray, 1))
 
 
 // Material
